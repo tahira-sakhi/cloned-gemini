3adc1b718c7ad03fbcfd840b664f02f475a44d88
tahira-sakhi <tahi.parveen93@gmail.com>
2024-05-15 13:30:20 +0500

    base url updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/cloned-gemini/",
+  base: "https://github.com/tahira-sakhi/cloned-gemini.git",
 });
